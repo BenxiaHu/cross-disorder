1b5934b522eeaddae0446af1179b641daf55e086
Benxia Hu <hubenxia@gmail.com>
2019-07-27 23:26:37 +0800

    Update rda2txt.R

--- rda2txt.R
+++ rda2txt.R
@@ -3,5 +3,3 @@ write.table(coloc_result_sig$locus2gene,file="/proj/hyejunglab/crossdisorder/Rev
 
 load("/proj/hyejunglab/eQTL/capstone4/eQTL/adultonly/eQTL_GRanges_colocinput.rda")
 write.table(qtlranges$gene,file="/proj/hyejunglab/crossdisorder/Revision/hubenxia/coloc_background.txt",sep="\t",quote=F,row.names=F)
-
-load("/proj/hyejunglab/crossdisorder/MAGMA/result_harper/magma1.07/fetal_brain/GO_FB/pval_0.05/GO_GSEA_protein-coding_genes_MHCextrm_BD.PGC.2019_FB.rda")
