7d944441c19fc14c17576a317ef698b94ca0f6ec
Benxia Hu <hubenxia@gmail.com>
2019-08-17 21:52:13 +0800

    Delete rda2txt.R

--- rda2txt.R
+++
@@ -1,5 +0,0 @@
-load("/proj/hyejunglab/disorder/Schizophrenia_clozuk/capstone4/coloc_outputfile_SCZ_Capstone4_cisQTL.rda")
-write.table(coloc_result_sig$locus2gene,file="/proj/hyejunglab/crossdisorder/Revision/hubenxia/coloc.txt",sep="\t",quote=F,row.names=F)
-
-load("/proj/hyejunglab/eQTL/capstone4/eQTL/adultonly/eQTL_GRanges_colocinput.rda")
-write.table(qtlranges$gene,file="/proj/hyejunglab/crossdisorder/Revision/hubenxia/coloc_background.txt",sep="\t",quote=F,row.names=F)
